2cd886e569bad705d75c82871243f87eee87a2f8
deviwax <156660735+deviwax@users.noreply.github.com>
2025-07-27 00:24:31 +0300

    modified

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -4,7 +4,7 @@ const API_KEY = '51470641-9c85038c1716f2f4dcdf71359';
 const BASE_URL = 'https://pixabay.com/api/';
 const PER_PAGE = 15;
 
-export async function getImagesByQuery(query) {
+export async function getImagesByQuery(query, page = 1) {
     const params = {
         key: API_KEY,
         q: query,
